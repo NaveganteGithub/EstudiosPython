9f97c174a15520607973ef11c99e65881bd524ff
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-05-29 19:12:40 +0200

    PG0000090

--- SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Examen/estudios/paquetes_anidados/mi_paquete/sub_paquete1/__init__.py
+++ SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Examen/estudios/paquetes_anidados/mi_paquete/sub_paquete1/__init__.py
@@ -1,3 +1,8 @@
 # mi_paquete/sub_paquete1/__init__.py
 print("Inicializando sub_paquete1...")
-from .modulo1 import funcion1
+
+# Primera forma
+# from .modulo1 import funcion1
+
+# Segunda forma
+from .modulo1 import *

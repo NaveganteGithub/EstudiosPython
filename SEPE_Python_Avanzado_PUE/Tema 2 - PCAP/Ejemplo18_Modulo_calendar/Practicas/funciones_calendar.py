e84271797695c32a2a6c73ed14249287a85a9228
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-06-11 21:29:55 +0200

    PG0000096

--- SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar.py
+++ SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar.py
@@ -19,7 +19,7 @@ calendar.prmonth(2024, 4)
 calendar.prmonth(2024, 4, w=4)
 calendar.prmonth(2024, 4, w=4, l=2)
 
-# Mostrar el canlendario comenzando la semana en domingo, se puede poner el numero correspondiente al dia de la semana
+# Mostrar el calendario comenzando la semana en domingo, se puede poner el numero correspondiente al dia de la semana
 calendar.setfirstweekday(6)
 calendar.setfirstweekday(calendar.SUNDAY) # Aunque es mejor trabajar con la constante
 calendar.prmonth(2024,4, w=4, l=2)

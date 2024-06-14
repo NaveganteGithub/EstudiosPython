602bbe1137221215b8af96530e271c76dfd86529
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-06-14 21:43:20 +0200

    PG0000098

--- SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar/funcion_leapdays.py
+++ SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar/funcion_leapdays.py
@@ -4,3 +4,8 @@ import calendar
 # Recordar que en los rangos el ultimo no esta incluido
 print("Cuantos bisiestos entre 2010 y 2024 sin incluir", calendar.leapdays(2010, 2024))
 print("Cuantos bisiestos entre 2010 y incluyendo el 2024", calendar.leapdays(2010, 2025))
+
+"""
+Cuantos bisiestos entre 2010 y 2024 sin incluir 3
+Cuantos bisiestos entre 2010 y incluyendo el 2024 4
+"""
--- SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar/funcion_weekday.py
+++ SEPE_Python_Avanzado_PUE/Tema 2 - PCAP/Ejemplo18_Modulo_calendar/Practicas/funciones_calendar/funcion_weekday.py
@@ -1,4 +1,4 @@
 import calendar
 
 # Dada una fecha retorna el dia de la semana en numero
-print(calendar.weekday(2024, 4, 17))
+print(calendar.weekday(2024, 4, 17)) # 2

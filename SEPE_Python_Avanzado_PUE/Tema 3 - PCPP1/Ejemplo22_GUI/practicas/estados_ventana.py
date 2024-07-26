fc8e831ecc0142a5cab9f6528d5ec64c1dbeb11a
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-07-26 20:50:23 +0200

    PG0000126

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/Ejemplo22_GUI/practicas/estados_ventana.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/Ejemplo22_GUI/practicas/estados_ventana.py
@@ -20,4 +20,23 @@ boton = tk.Button(ventana, text="Minimizar ventana", command=minimizar)
 boton.pack()
 
 
+# Puedes utilizar el Administrador de Tareas de Windows para ver los procesos
+# en segundo plano, el proceso en segundo plano de este script se llama "Python",
+# puedes finalizarlo si quieres desde el administrador de tareas
+def segundo_plano():
+    ventana.state("withdrawn")
+
+
+boton = tk.Button(ventana, text="Segundo Plano", command=segundo_plano)
+boton.pack()
+
+
+def normal():
+    ventana.state("normal")
+
+
+boton = tk.Button(ventana, text="Por defecto", command=normal)
+boton.pack()
+
+
 ventana.mainloop()

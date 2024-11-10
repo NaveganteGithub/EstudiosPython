0853f992f117e8e54b4548b2a5d36a15e52362b5
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-11-10 09:39:12 +0100

    PCAP and PCPP studies oriented to fix a comment in the line of the Pass to admin

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s4_ficheros/r6_Configuraciones.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s4_ficheros/r6_Configuraciones.py
@@ -3,6 +3,8 @@ import configparser
 config = configparser.ConfigParser()
 
 config['default'] = {"User": "preparatoria", "Pass": "asd1w2·"}
+# En el campo Pass habia un % antes del segundo 5, pero daba
+# un ValueError por lo que tuve que quitarlo
 config['admin'] = {"User": "administrator", "Pass": "54f·g $d 56g -.4e6-4  g+5- e41 w6 g4"}
 
 with open("./archivo.ini", "wt", encoding="utf-8") as archivo:

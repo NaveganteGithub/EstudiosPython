e8167e5af4ca738af0ce72ee64bba3f337b772d9
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-11-10 09:53:41 +0100

    PCAP and PCPP studies oriented to fix the configparser example

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s4_ficheros/r6_Configuraciones.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s4_ficheros/r6_Configuraciones.py
@@ -5,8 +5,17 @@ config = configparser.ConfigParser()
 config['default'] = {"User": "preparatoria", "Pass": "asd1w2·"}
 # En el campo Pass habia un % antes del segundo 5, pero daba
 # un ValueError por lo que tuve que quitarlo
-config['admin'] = {"User": "administrator", "Pass": "54f·g $d 56g -.4e6-4  g+5- e41 w6 g4"}
+config['admin'] = {"User": "administrator",
+                   "Pass": "54f·g $d 56g -.4e6-4  g+5- e41 w6 g4"
+                   }
 
 with open("./archivo.ini", "wt", encoding="utf-8") as archivo:
     config.write(archivo)
 
+config.read("./archivo.ini", encoding="utf-8")
+
+print(config['admin']['Pass'])
+
+mi_config = config['admin']
+for conf in mi_config:
+    print(mi_config[conf])

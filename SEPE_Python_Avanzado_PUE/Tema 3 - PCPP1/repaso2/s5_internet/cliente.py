f695ee98eb3e3e3ea6776774f1e38f65b244aa68
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-11-11 21:53:45 +0100

    Modification to fix a bug that produce an loop infinity

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s5_internet/cliente.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s5_internet/cliente.py
@@ -1,6 +1,5 @@
 import socket
 
-
 LONGITUDMENSAJE = 10
 
 mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,28 +9,24 @@ datos_red = tuple([socket.gethostname(), 50307])
 try:
     mi_socket.connect(datos_red)
 except ConnectionRefusedError:
-    exit(2)
+    exit("Conexion rechazada, saliendo del cliente.")
 
 mensaje = ""
-
+es_nuevo = True
+longitud_cabecera = None
 
 while True:
 
-    es_nuevo = True
-    longitud_cabecera = None
+    recibir_paquete = mi_socket.recv(16)
 
-    while True:
+    if es_nuevo:
+        longitud_cabecera = int(recibir_paquete[:LONGITUDMENSAJE])
+        es_nuevo = False
 
-        recibir_paquete = mi_socket.recv(16)
+    mensaje += recibir_paquete.decode("utf-8")
 
-        if es_nuevo:
-            longitud_cabecera = int(recibir_paquete[:LONGITUDMENSAJE])
-            es_nuevo = False
-
-        mensaje += recibir_paquete.decode("utf-8")
-
-        if len(mensaje) - LONGITUDMENSAJE == longitud_cabecera:
-            print(mensaje)
-            es_nuevo = False
-            longitud_cabecera = None
-
+    if len(mensaje) - LONGITUDMENSAJE == longitud_cabecera:
+        print(mensaje)
+        es_nuevo = False
+        longitud_cabecera = None
+        break

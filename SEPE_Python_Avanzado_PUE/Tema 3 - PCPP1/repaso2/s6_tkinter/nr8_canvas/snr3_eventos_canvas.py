35fa459513d1a2e952ed8c1a387185fa202725f9
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-06 21:58:49 +0100

    Events to canvas

---
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s6_tkinter/nr8_canvas/snr3_eventos_canvas.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+def toggle_state(event):
+    if canvas.itemcget(line, "state") == "disabled":
+        canvas.itemconfig(line, state="normal")
+    else:
+        canvas.itemconfig(line, state="disabled")
+
+root = tk.Tk()
+canvas = tk.Canvas(root, width=400, height=400)
+canvas.pack()
+
+# Dibuja una línea con parámetros deshabilitados
+line = canvas.create_line(50, 50, 200, 200, fill="blue", width=2, dash=(4, 2), disabledfill="gray", disabledwidth=1, disableddash=(1, 1))
+
+# Vincular evento para alternar estado de la línea
+canvas.bind("<Button-1>", toggle_state)
+
+root.mainloop()

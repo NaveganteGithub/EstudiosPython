a6f9f238fd8f34196028a03da90b4145aed995e1
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-18 23:08:51 +0100

    Init notes BD and update notes PEP8

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s7_PEP/r3_PEP8_guia_estilo/snr4_operadores.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s7_PEP/r3_PEP8_guia_estilo/snr4_operadores.py
@@ -1,1 +1,25 @@
-# Segun Donald Knuth es
+# Según Donald Knuth el código es mucho más legible si
+# se hace un salto de línea antes de cada operador.
+
+manzanas = 8
+platano = 9
+peras = 10
+naranjas = 25
+albaricoque = 2
+coco = 8
+racimos_uvas = 70
+
+manzanas_vendidas = 2
+peras_vendidas = 3
+
+oferta = 2
+
+frutas_disponibles = ((manzanas - manzanas_vendidas)
+                      + platano
+                      + (peras - peras_vendidas)
+                      + naranjas
+                      + albaricoque
+                      + coco
+                      + racimos_uvas
+                      * oferta)
+

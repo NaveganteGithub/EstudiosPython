d81452e539f5e2e37eeead5d9e6144bc0d28a918
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-23 18:43:49 +0100

    Documentation permissions r3_serializacion

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r3_serializacion.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r3_serializacion.py
@@ -1,11 +1,16 @@
 import shelve
 
+# r - READ
+# w - WRITE
+# c - CREATING (DEFAULT) - Utiliza la existente, sino existe la crea
+# n - NEW - Siempre crea la base de datos vacia
+
 fichero = shelve.open("./mis_datos", "n")
 
 fichero['DEFAULT'] = {1,5,6,8,52}
 fichero['opcion1'] = {4,5,8,9,2}
 
-fichero.close()
+fichero.close()  # Output: DIR, DAT, BAK
 
 fichero = shelve.open("mis_datos", "r")
 print(fichero['opcion1'])

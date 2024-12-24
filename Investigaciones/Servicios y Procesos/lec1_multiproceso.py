e0872a56b1d21d8bf308f1121393876d7753c80e
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-24 19:02:18 +0100

    Multiprocess programming with example three process execution

---
+++ Investigaciones/Servicios y Procesos/lec1_multiproceso.py
@@ -0,0 +1,31 @@
+from multiprocessing import Process
+from time import sleep
+
+
+def funcion_nombre(nombre):
+    while True:
+        print('Hola', nombre)
+        sleep(7)
+
+
+def funcion_nombre_corto(nombre):
+    while True:
+        print('Hola', nombre)
+        sleep(1)
+
+
+if __name__ == '__main__':
+
+    # Los procesos y los subprocesos son independientes, los hilos no
+
+    proceso1 = Process(target=funcion_nombre,     # Función
+                       args=('Maria',))           # Parámetros de la función
+    proceso1.start()
+
+    proceso2 = Process(target=funcion_nombre, args=('David',))
+    proceso2.start()
+
+    proceso3 = Process(target=funcion_nombre_corto, args=('Daniel',))
+    proceso3.start()
+
+    proceso3.join()   # Proceso principal en modo espera hasta que el proceso3 termine

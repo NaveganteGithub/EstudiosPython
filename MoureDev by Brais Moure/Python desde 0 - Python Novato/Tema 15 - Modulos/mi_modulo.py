fc206ef0ee791f43afbada135eb7b50eb859aed2
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-25 11:54:53 +0100

    Delete MoureDev

--- MoureDev by Brais Moure/Python desde 0 - Python Novato/Tema 15 - Modulos/mi_modulo.py
+++
@@ -1,3 +0,0 @@
-
-def sum(n_One, n_Two, n_Three):
-    print(n_One + n_Two + n_Three)
--- MoureDev by Brais Moure/Python desde 0 - Python Novato/Tema 15 - Modulos/modulo.py
+++
@@ -1,6 +0,0 @@
-
-def sumValue(n_One, n_Two, n_Three):
-    print(n_One + n_Two + n_Three)
-    
-def printValue(value):
-    print(value)

8027d3b64bd92d2717d760728ddabbe5847dd8f0
Ismael Montoro Peñasco <ismaelmontorop@protonmail.com>
2024-12-27 19:42:46 +0100

    Update notes r2_atributos and r3_acceso

--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r12_sobreescritura/r2_atributos.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r12_sobreescritura/r2_atributos.py
@@ -5,28 +5,45 @@ class Atributos:
         self.mi_nombre = nombre
         self.mi_edad = edad
 
+    # getattr tiene dos posibilidades, que encuentre o
+    # no encuentre el atributo a procesar
+
+    # en caso de encontrar el atributo procesará esta logica
     def __getattribute__(self, item):
         print(f"Has pedido {item}")
         valor = super().__getattribute__(item)
         return valor
 
+    # en caso de no encontrar el atributo procesará esta logica
     def __getattr__(self, item):
         return f"{item} no existe"
 
+    # Modifica el comportamiento del método setattr
     def __setattr__(self, clave, valor):
         print(f"{clave} a cambiado su valor a {valor}")
         super().__setattr__(clave, valor)
 
+    # Modifica el comportamiento del método delattr
     def __delattr__(self, item):
         valor = super().__getattribute__(item)
         super().__delattr__(item)
         print(f"{item} tenia el valor {valor}")
 
+
+# El constructor como tiene inserciones a variables
+# de instancia el método setattr se activara
 clase = Atributos("Alejandro", 52)
+print("-" * 20)
+
 print(getattr(clase, "mi_nombre"))
+print("-" * 20)
+
 print(getattr(clase, "mi_nomb"))
+print("-" * 20)
 
 setattr(clase, "mi_nombre", "Lorena")
 print(getattr(clase, "mi_nombre"))
+print("-" * 20)
 
 delattr(clase, "mi_nombre")
+print("-" * 20)
--- SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r12_sobreescritura/r3_acceso.py
+++ SEPE_Python_Avanzado_PUE/Tema 3 - PCPP1/repaso2/s2_poo/r12_sobreescritura/r3_acceso.py
@@ -1,17 +1,16 @@
-import random
-
 
 class Acceso:
 
     def __init__(self, nombre, edad):
         self.mi_nombre = nombre
         self.mi_edad = edad
-        self.codigo = list(str(random.randint(1000, 10000)))
 
+    # getitem permite modificar el modo de acceder a las variables
     def __getitem__(self, item):
         print(f"{item} a sido pedido")
         return exec(f"self.{item}")
 
+    # setitem permite modificar el comportamiento de las inserciones
     def __setitem__(self, clave, valor):
         print(f"{clave} a cambiado su valor {valor}")
         if type(valor) is str:
@@ -19,33 +18,48 @@ class Acceso:
         else:
             exec(f"self.{clave} = {valor}")
 
+    # delitem permite modificar el comportamiento de la palabra reservada del
     def __delitem__(self, clave):
         print(f"Borrando {clave}")
         exec(f"del self.{clave}")
 
+    # Permite modificar el comportamiento de la función len
     def __len__(self):
         return len(self.mi_nombre)
 
+    # Permite realizar iteradores personalizados
+    # para cada instancia de clase
     def __iter__(self):
-        return iter(self.codigo)
+        return iter(list(self.mi_nombre))
 
+    # Permite modificar comportamiento de la palabra reservada "in"
+    # ante las instancias de la clase
     def __contains__(self, item):
         print(f"Analizando {item}")
         return item in self.mi_nombre
 
 clase = Acceso("Daniel", 21)
+print("-" * 20)
+
 print(clase["mi_nombre"])
+print("-" * 20)
 
 clase["mi_nombre"] = "David"
+print("-" * 20)
+
 print(clase["mi_nombre"])
+print("-" * 20)
 
 print(len(clase))
+print("-" * 20)
 
 print("a" in clase)
+print("-" * 20)
 
 for l in clase:
-    print(l, end="")
+    print(l, end=".")
 
 print()
+print("-" * 20)
 
 del clase["mi_nombre"]
